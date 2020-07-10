26c5636b92d89bfafa664fbde8962877a7a1cd5f
ashalexis <ashalexis-1297@users.noreply.github.com>
2020-07-10 18:14:55 +1000

    files updated

---
+++ build/precache-manifest.a8ec2a9b13450c92c8afb8256ce35494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe2b3aa1eec427ca6edca9ce7e90746",
+    "url": "/random-color-picker/index.html"
+  },
+  {
+    "revision": "6395e01278e7e330c05b",
+    "url": "/random-color-picker/static/css/main.835e7ff7.chunk.css"
+  },
+  {
+    "revision": "ae51581cce3e8747c715",
+    "url": "/random-color-picker/static/js/2.ade38f2c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/random-color-picker/static/js/2.ade38f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6395e01278e7e330c05b",
+    "url": "/random-color-picker/static/js/main.b459a53a.chunk.js"
+  },
+  {
+    "revision": "5c75e191f0317ba3ef3e",
+    "url": "/random-color-picker/static/js/runtime-main.fd318265.js"
+  }
+]);
